bfa7c41002578f5d3327dcf1adf2ac769cbe9710
hojun313 <hojun313@naver.com>
2025-06-13 17:48:34 +0900

    실습 12 진행

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,14 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    # 아래 한 줄 추가
+    image = models.ImageField(upload_to='intruder_image/%Y/%m/%d/')
 
     def publish(self):
         self.published_date = timezone.now()
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,14 @@ print("--- mysite urls.py 파일이 로드되었습니다! ---")
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
